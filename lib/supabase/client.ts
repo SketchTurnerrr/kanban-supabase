d144e68b78318e216fa725ccf791939ccab46c5d
SketchTurnerrr <nareknarek45@gmail.com>
2024-03-25 21:09:56 +0200

    fix: stupid env wont work, hardcoding it in

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,6 +3,6 @@ import { Database } from '../../types/supabase';
 
 export const createClient = () =>
   createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    'https://giwjjkxqwklczyuzdbmt.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdpd2pqa3hxd2tsY3p5dXpkYm10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwNDY0MjQsImV4cCI6MjAyNjYyMjQyNH0.39Eoy-ySxtLpzdqEDxWRiRwhkiiFwXRf5pvRM1c_AHE'
   );
